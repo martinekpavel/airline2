c18bc62a2526e0ab222f3a78ad98e9e28b51aa64
Pavel Martínek <martinek.pavel@oa-opava.cz>
2025-05-02 09:42:57 +0200

    add image

--- flights/models.py
+++ flights/models.py
@@ -12,6 +12,9 @@ class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+
+    # https://www.youtube.com/watch?v=O5YkEFLXcRg
 
     def __str__(self):
         return f"id {self.id}: {self.origin} --> {self.destination} délka letu ({self.duration})"
